6311c53775831ebdb49f0a690888f6bc3c5a21dd
YuaVN <95994365+YuaVN@users.noreply.github.com>
2022-05-01 14:08:09 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,13 +9,14 @@ echo "Select minecraft server version (only 1.8)"
 echo "1.NachoSpigot"
 echo "2.Spigot"
 echo "3.PaperMC"
+echo "4.Bungeecord"
 
 read version
 
 paperlink="https://papermc.io/api/v2/projects/paper/versions/1.8.8/builds/445/downloads/paper-1.8.8-445.jar"
 nacholink="https://transfer.sh/sTU9t8/NachoSpigot.jar"
 spigotlink="https://cdn.getbukkit.org/spigot/spigot-1.8.8-R0.1-SNAPSHOT-latest.jar"
-
+bungeelink="https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/bootstrap/target/BungeeCord.jar/*view*/"
 
  if [ $version = 1 ];
     then
@@ -38,3 +39,9 @@ fi
     cd PaperServer
     wget $paperlink
 fi
+
+ if [ $version = 4 ];
+    then
+    mkdir BungeeServer
+    cd BungeeServer
+    wget $bungeelink
